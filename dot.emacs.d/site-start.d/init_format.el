6fadc87d6e183622aa5d8348bee5bc79d446a331
kikeg <kikeg@kikeg.com>
2026-08-24 11:08:24 +0200

    Added rustfmt edition parameter.

--- dot.emacs.d/site-start.d/init_format.el
+++ dot.emacs.d/site-start.d/init_format.el
@@ -4,4 +4,8 @@
   ;; Enable it globally (it handles most languages out of the box)
   (apheleia-global-mode +1))
 
+(setf (alist-get 'rust apheleia-formatters)
+      '("rustfmt" "--edition" (or (bound-and-true-p rust-edition) "2024")
+        "--quiet" "--emit" "stdout"))
+
 (provide 'init_format)
